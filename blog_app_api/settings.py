600177b06a97e6e55876eb78f5ea454e944f3f3f
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2021-08-04 14:23:31 +0700

    install token auth depends

--- blog_app_api/settings.py
+++ blog_app_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    # 'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.TokenAuthentication'
+#     ],
+# }
 
 ROOT_URLCONF = 'blog_app_api.urls'
 
