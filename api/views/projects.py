2627a573cc84078ba923eb73a2da7ecbeb32ac63
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2021-08-05 22:53:23 +0700

    refactor function update_or_create_tags, add class permission

--- api/views/projects.py
+++ api/views/projects.py
@@ -2,8 +2,30 @@ import re
 from rest_framework import viewsets, serializers, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_access_policy import AccessPolicy
 
 from api.models import Project, Tag
+
+class ProjectsAccessPolicy(AccessPolicy):
+    statements = [
+        {
+            "action": ["list", "retrieve"],
+            "principal": "*",
+            "effect": "allow"
+        },
+        {
+            "action": ["create", 'update', 'destroy'],
+            "principal": '*',
+            "effect": "allow",
+            "condition": "is_allow_create"
+        }
+    ]
+
+    def is_allow_create(self, request, view, action):
+        user = request.user
+        isAuth = user.is_authenticated
+        return isAuth
+
 class ProjectSerializer(serializers.ModelSerializer):
 
     tags = serializers.SerializerMethodField()
@@ -31,17 +53,23 @@ class ProjectsViewset(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'put', 'delete']
     queryset = Project.objects.all().order_by('-created')
     serializer_class = ProjectSerializer
+    permission_classes = (ProjectsAccessPolicy,)
 
-    def create(self, request, *args, **kwargs):
-        data = request.data
-        tags = data.get('tags', None)
-
+    def update_or_create_tags(self, tags):
         tag_list = []
 
         if tags is not None:
             for tag in tags:
                 tag_obj, created = Tag.objects.get_or_create(name=tag)
                 tag_list.append(tag_obj)
+        
+        return tag_list
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        tags = data.get('tags', None)
+
+        tag_list = self.update_or_create_tags(tags)
 
         project = Project.objects.create(
             title=data['title'],
@@ -59,15 +87,11 @@ class ProjectsViewset(viewsets.ModelViewSet):
         project = Project.objects.filter(id=pk).first()
         data = request.data
         tags = data.get('tags', None)
-        tag_list = []
 
         if project is None:
             return Response({ "message": "project not found " }, status=status.HTTP_400_BAD_REQUEST)
 
-        if tags is not None:
-            for tag in tags:
-                tag_obj, created = Tag.objects.get_or_create(name=tag)
-                tag_list.append(tag_obj)
+        tag_list = self.update_or_create_tags(tags)
 
         project = Project.objects.create(
             title=data['title'],
