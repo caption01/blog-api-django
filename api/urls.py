92bc7f064a73c1a0b73fe727070bc2b0ea764d90
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2021-08-05 15:00:37 +0700

    add login apiview

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.urls import path
 from .views.profile import ProfileView
+from .views.login import LoginView
 
 urlpatterns = [
+    url(r'login/?', LoginView.as_view()),
     url(r'^profile(?:/(?P<pk>[^/.]+))?/?', ProfileView.as_view())
 ]
